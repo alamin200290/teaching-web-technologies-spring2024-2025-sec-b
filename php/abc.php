47551b7d332b1e72f39e1e5a4fd9dafb30025cd6
Md.Al-Amin <amin.se.aiub@gmail.com>
2025-04-28 16:37:03 +0600

    Add files via upload

---
+++ php/abc.php
@@ -0,0 +1,17 @@
+<?php
+
+    if(isset($_POST['submit'])){
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+
+        if($username == "" || $password == ""){
+            echo "null username/password!";
+        }else if($username == $password){
+            echo "valid user!";
+        }else{
+            echo "invalid user!";
+        }
+    }else{
+        echo "Invalid request! Please submit form!";
+    }
+?>
